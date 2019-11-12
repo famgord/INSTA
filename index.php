ee8c594d2d7e9677c2392b168a72b2be05329413
famgord <FAMGORD>
2019-11-12 11:54:10 +0700

    begin

--- index.php
+++ index.php
@@ -1,2 +1,1 @@
-<?php
-echo "<b>"."Hello"."<br>";
+//https://www.instagram.com/oauth/authorize?app_id=562565477851628&redirect_uri=https://78.47.48.188/auth/&scope=user_profile,user_media&response_type=code
