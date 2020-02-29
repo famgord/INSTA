5cf3eb1e60e50a76e54dba7248471ca598530c74
An.Gord <198616ltg>
2020-02-29 23:17:47 +0700

    inputs + content.php

---
+++ content.php
@@ -0,0 +1,3 @@
+<?php
+$a = $_POST['account'];
+$b = $_POST['countImage'];
--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 //https://www.instagram.com/oauth/authorize?app_id=562565477851628&redirect_uri=https://78.47.48.188/auth/&scope=user_profile,user_media&response_type=code
-Примеhshdjkahslkjd
+
+<form action="content.php" method="post">
+    <input type="text" name="account" placeholder="Account">
+    <input type="text" name="countImage" placeholder="Count">
+</form>
