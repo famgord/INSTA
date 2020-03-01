58760445063c17d61fa68b31ec13e62fddb42ce2
An.Gord <198616ltg>
2020-03-01 20:17:19 +0700

    begin api insta

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-//https://www.instagram.com/oauth/authorize?app_id=562565477851628&redirect_uri=https://78.47.48.188/auth/&scope=user_profile,user_media&response_type=code
-
+<?php   require ('header.php'); ?>
+<link rel='stylesheet' href='css/style.css'>
 <form action="content.php" method="post">
-    <input type="text" name="account" placeholder="Account">
-    <input type="text" name="countImage" placeholder="Count">
+    <div class="center">
+        <input type="text" name="account" placeholder="Account"></br>
+        <input type="text" name="countImage" placeholder="Count"></br>
+        <input type="submit" name="submit">
+    </div>
 </form>
+<?php
