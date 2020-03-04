fc884870991d4d7f2e9b11eee85730e2cf80b875
An.Gord <198616ltg>
2020-03-04 21:47:17 +0700

    перебор записей

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<link rel='stylesheet' href='css/style.css'>
 <?php   require ('header.php');
 //<link rel='stylesheet' href='css/style.css'>
 //<form action="content.php" method="post">
@@ -25,36 +26,25 @@ function object_to_array($data)
 $arr = array();
 
 $group_id = -57169109;
-$count_post = 1;
+$count_post = 10;
 $request_params = array(
 'owner_id' => $group_id,
 'count' => $count_post,
 'filter' => 'owner',
 'extended' => '0',
-'v' => '5.103',
+'v' => '5.52',
 'access_token' => 'f802f98a7fbb64c4ced99638ef06832ba5c7d8a7b0906785b178d1146e2296f313f6039f08a32cd83e918'
 );
 $get_params = http_build_query($request_params);
 $result = json_decode(file_get_contents('https://api.vk.com/method/wall.get?' . $get_params));
-var_dump($result);
-echo '<br><br>';
 $arr = object_to_array($result);
-var_dump($arr);
-echo '<br><br>';
-$arr1 = $arr['response']['items'][0]['attachments'][1]['doc']['url'];
-echo $arr1;
 
-
-//$user_id = 210700286;
-//$request_params = array(
-//	'user_id' => $user_id,
-//	'fields' => 'bdate',
-//	'v' => '5.52',
-//	'access_token' => 'f802f98a7fbb64c4ced99638ef06832ba5c7d8a7b0906785b178d1146e2296f313f6039f08a32cd83e918'
-//);
-//$get_params = http_build_query($request_params);
-//$result = json_decode(file_get_contents('https://api.vk.com/method/users.get?' . $get_params));
-//echo (gettype($result));
-//echo($result->response[0]->bdate);
+for ($i=0; $i<=$count_post; $i++) {
+    $doc_title = $arr['response']['items'][$i]['attachments'][1]['doc']['title'];
+    $doc_url = $arr['response']['items'][$i]['attachments'][1]['doc']['url'];
+    $doc_size = $arr['response']['items'][$i]['attachments'][1]['doc']['size'];
+    echo $i. "<br>";
+    echo $doc_title."<br>". $doc_url."<br>". round($doc_size/1000000,2)." mb <br>";
+}
 
 
